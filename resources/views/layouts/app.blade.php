d7056740478e8d55ec8f50ba6865fd8812c22a84
Gabor Ivanyiczki <gabor.ivanyiczki@programming-pool.com>
2020-03-09 17:22:39 +0200

    Add navbar items

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{ URL::to('/') }}/images/favicon/5.png" type="image/x-icon">
     <link rel="shortcut icon" href="{{ URL::to('/') }}/images/favicon/5.png" type="image/x-icon">
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>Night & Day by 4Brands Official - Website</title>
 
     <!--Google font-->
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
@@ -415,77 +415,13 @@
                                 </ul>
                             </li>
                             <li>
-                                <a href="#">Produse</a>
-                                <ul>
-                                    <li>
-                                        <a href="#">sidebar</a>
-                                        <ul>
-                                            <li><a href="product-page.html">left sidebar</a></li>
-                                            <li><a href="product-page(right-sidebar).html">right sidebar</a></li>
-                                            <li><a href="product-page(no-sidebar).html">no sidebar</a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#">thumbnail image</a>
-                                        <ul>
-                                            <li><a href="product-page(left-image).html">left image</a></li>
-                                            <li><a href="product-page(right-image).html">right image</a></li>
-                                            <li><a href="product-page(image-outside).html">image outside <span class="new-tag">new</span></a></li>
-                                        </ul>
-                                    </li>
-                                    <li>
-                                        <a href="#">three column</a>
-                                        <ul>
-                                            <li><a href="product-page(left-image).html">thumbnail left</a></li>
-                                            <li><a href="product-page(right-image).html">thumbnail right</a></li>
-                                            <li><a href="product-page(image-outside).html">thubnail bottom</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="product-page(4-image).html">four image <span class="new-tag">new</span></a></li>
-                                    <li><a href="product-page(sticky).html">sticky</a></li>
-                                    <li><a href="product-page(accordian).html">accordian</a></li>
-                                    <li><a href="product-page(bundle).html">bundle<span class="new-tag">new</span></a></li>
-                                    <li><a href="product-page(image-swatch).html">image swatch <span class="new-tag">new</span></a></li>
-                                    <li><a href="product-page(vertical-tab).html">vertical tab</a></li>
-                                </ul>
+                                <a href="#">noutati</a>
                             </li>
-                            <li><a href="#">pages</a>
-                                <ul>
-                                    <li>
-                                        <a href="#">account</a>
-                                        <ul>
-                                            <li><a href="product-page(left-image).html">account <span class="new-tag">new</span></a></li>
-                                            <li><a href="product-page(right-image).html">order history <span class="new-tag">new</span></a></li>
-                                            <li><a href="wishlist.html">wishlist</a></li>
-                                            <li><a href="cart.html">cart</a></li>
-                                            <li><a href="dashboard.html">Dashboard</a></li>
-                                            <li><a href="login.html">login</a></li>
-                                            <li><a href="register.html">register</a></li>
-                                            <li><a href="contact.html">contact</a></li>
-                                            <li><a href="forget_pwd.html">forget password</a></li>
-                                            <li><a href="profile.html">profile <span class="new-tag">new</span></a></li>
-                                            <li><a href="checkout.html">checkout</a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="about-page.html">about us</a></li>
-                                    <li><a href="search.html">search</a></li>
-                                    <li><a href="typography.html">typography <span class="new-tag">new</span></a></li>
-                                    <li><a href="review.html">review <span class="new-tag">new</span></a></li>
-                                    <li><a href="order-success.html">order success</a></li>
-                                    <li>
-                                        <a href="#">compare</a>
-                                        <ul>
-                                            <li><a href="compare.html">compare</a></li>
-                                            <li><a href="compare-2.html">compare-2 <span class="new-tag">new</span></a></li>
-                                        </ul>
-                                    </li>
-                                    <li><a href="collection.html">collection</a></li>
-                                    <li><a href="lookbook.html">lookbook</a></li>
-                                    <li><a href="sitemap.html">site map <span class="new-tag">new</span></a></li>
-                                    <li><a href="404.html">404</a></li>
-                                    <li><a href="coming-soon.html">coming soon <span class="new-tag">new</span></a></li>
-                                    <li><a href="faq.html">FAQ</a></li>
-                                </ul>
+                            <li>
+                                <a href="#">despre noi</a>
+                            </li>
+                            <li>
+                                <a href="#">contact</a>
                             </li>
                             <li>
                                 <a href="#">make your own</a>
